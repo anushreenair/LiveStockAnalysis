54fde5d4014366d84261ae5f3d8ffa783be91c03
Anushree Nair <97722918+anushreenair@users.noreply.github.com>
2025-04-04 19:09:49 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,13 +9,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCctf7IJr4LZXamfuNDCmYo5VJvhODTCJg",
-  authDomain: "projectstockwizard.firebaseapp.com",
-  projectId: "projectstockwizard",
-  storageBucket: "projectstockwizard.firebasestorage.app",
-  messagingSenderId: "1046255423842",
-  appId: "1:1046255423842:web:89238c59c848b1db625c3b",
-  measurementId: "G-ZGVCVWVXK2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
